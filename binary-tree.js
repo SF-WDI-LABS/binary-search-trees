aa4458fb645d1b79296ebba42d1270356575328b
Esther Weon <esther.s.weon@gmail.com>
2018-02-07 12:53:33 -0800

    Address PR

--- binary-tree.js
+++ binary-tree.js
@@ -9,16 +9,55 @@ function BinarySearchTree(root) {
 }
 
 BinarySearchTree.prototype.insertNode = insertNode;
-BinarySearchTree.prototype.includesNode = includesNode;
+BinarySearchTree.prototype.checkIfIncludesNode = checkIfIncludesNode;
 
 // INPUTS: BinaryNode to start at (BinarySearchTree's root), number to insert into the BinarySearchTree
 // OUTPUT: None (no return necessary), but `console.log`ing the BinarySearchTree should show an updated tree
-function insertNode(currentNode, numToInsert) {
+function insertNode(numToInsert) {
+	// If the BinarySearchTree has no root, set it to a BinaryNode with numToInsert as its value
+	if (!this.root) {
+		this.root = new BinaryNode(numToInsert);
+
+	// Otherwise, recursively go down the BinarySearchTree to see where to insert the numToInsert
+	} else {
+		recursivelyInsertNode(this.root, numToInsert);
+	}
 }
+
+function recursivelyInsertNode(currentNode, numToInsert) {
+	// Compare your currentNode's value to numToInsert to see which direction to go
+
+	// If the next BinaryNode in that direction is empty, insert here
+
+	// Otherwise, keep recursively going down the BinarySearchTree
+}
+
 
 // INPUTS: BinaryNode to start at (BinarySearchTree's root), number to find in the BinarySearchTree
 // OUTPUT: Boolean value representing whether number was found
-function includesNode(currentNode, numToFind) {
+function checkIfIncludesNode(numToFind) {
+	// If this BinarySearchTree has no root, this tree cannot contain numToFind
+	if (!this.root) {
+
+	// Else, if this BinarySearchTree's root matches numToFind, this tree does contain numToFind
+	} else if (this.root.value === numToFind) {
+
+	//  Otherwise, recursively go down the BinarySearchTree to check whether the tree contains numToFind
+	} else {
+		return recursivelyCheckIfIncludesNode(this.root, numToFind);
+	}
+}
+
+function recursivelyCheckIfIncludesNode(currentNode, numToFind) {
+	// Compare your currentNode's value to numToInsert to see which direction to go
+
+	// Before you go down either direction, check if the currentNode's value is equal to numToFind
+	// If it is, you've finished – this tree does contain numToFind
+
+	// Otherwise, check if the next BinaryNode in the correct direction is empty
+	// If it is, this tree does not contain numToFind
+
+	// If neither of those conditions are true, keep recursively going down the BinarySearchTree
 }
 
 
@@ -32,10 +71,17 @@ function includesNode(currentNode, numToFind) {
 // let myBinarySearchTree = new BinarySearchTree(myRootNode);
 
 // *****BINARY TREE AFTER INSERTIONS*****
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 12);
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 14);
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 16);
+// myBinarySearchTree.insertNode(12);
+// myBinarySearchTree.insertNode(14);
+// myBinarySearchTree.insertNode(16);
+
+// let emptyBinarySearchTree = new BinarySearchTree();
+// emptyBinarySearchTree.insertNode(1);
+// emptyBinarySearchTree.insertNode(2);
 
 // *****FINDING THINGS IN BINARY TREE*****
-// myBinarySearchTree.includesNode(myBinarySearchTree.root, 14);
-// myBinarySearchTree.includesNode(myBinarySearchTree.root, 2);
+// myBinarySearchTree.checkIfIncludesNode(16);
+// myBinarySearchTree.checkIfIncludesNode(100);
+
+// let anotherEmptyBinarySearchTree = new BinarySearchTree();
+// anotherEmptyBinarySearchTree.checkIfIncludesNode(5);
--- solution/binary-tree.js
+++ solution/binary-tree.js
@@ -9,37 +9,70 @@ function BinarySearchTree(root) {
 }
 
 BinarySearchTree.prototype.insertNode = insertNode;
-BinarySearchTree.prototype.includesNode = includesNode;
+BinarySearchTree.prototype.checkIfIncludesNode = checkIfIncludesNode;
 
 // INPUTS: BinaryNode to start at (BinarySearchTree's root), number to insert into the BinarySearchTree
 // OUTPUT: None (no return necessary), but `console.log`ing the BinarySearchTree should show an updated tree
-function insertNode(currentNode, numToInsert) {
-	if (!currentNode) {
-		currentNode = new BinaryNode(numToInsert, null, null);
+function insertNode(numToInsert) {
+	// If the BinarySearchTree has no root, set it to a BinaryNode with numToInsert as its value
+	if (!this.root) {
+		this.root = new BinaryNode(numToInsert);
+
+	// Otherwise, recursively go down the BinarySearchTree to see where to insert the numToInsert
 	} else {
-		let direction = numToInsert < currentNode.value ? 'left' : 'right';
-		if (!currentNode[direction]) {
-			currentNode[direction] = new BinaryNode(numToInsert, null, null);
-		} else {
-			insertNode(currentNode[direction], numToInsert);
-		}
+		recursivelyInsertNode(this.root, numToInsert);
 	}
 }
 
+function recursivelyInsertNode(currentNode, numToInsert) {
+	// Compare your currentNode's value to numToInsert to see which direction to go
+	let direction = numToInsert < currentNode.value ? 'left' : 'right';
+
+	// If the next BinaryNode in that direction is empty, insert here
+	if (!currentNode[direction]) {
+		currentNode[direction] = new BinaryNode(numToInsert);
+
+	// Otherwise, keep recursively going down the BinarySearchTree
+	} else {
+		recursivelyInsertNode(currentNode[direction], numToInsert);
+	}
+}
+
+
 // INPUTS: BinaryNode to start at (BinarySearchTree's root), number to find in the BinarySearchTree
 // OUTPUT: Boolean value representing whether number was found
-function includesNode(currentNode, numToFind) {
-	if (!currentNode) {
+function checkIfIncludesNode(numToFind) {
+	// If this BinarySearchTree has no root, this tree cannot contain numToFind
+	if (!this.root) {
 		return false;
-	} else if (currentNode.value == numToFind) {
+
+	// Else, if this BinarySearchTree's root matches numToFind, this tree does contain numToFind
+	} else if (this.root.value === numToFind) {
 		return true;
+
+	//  Otherwise, recursively go down the BinarySearchTree to check whether the tree contains numToFind
 	} else {
-		let direction = numToFind < currentNode.value ? 'left' : 'right';
-		if (!currentNode[direction]) {
-			return false;
-		} else {
-			return includesNode(currentNode[direction], numToFind);
-		}
+		return recursivelyCheckIfIncludesNode(this.root, numToFind);
+	}
+}
+
+function recursivelyCheckIfIncludesNode(currentNode, numToFind) {
+	// Compare your currentNode's value to numToInsert to see which direction to go
+	let direction = numToFind < currentNode.value ? 'left' : 'right';
+
+	// Before you go down either direction, check if the currentNode's value is equal to numToFind
+	// If it is, you've finished – this tree does contain numToFind
+	if (currentNode.value === numToFind) {
+		return true;
+
+	// Otherwise, check if the next BinaryNode in the correct direction is empty
+	// If it is, this tree does not contain numToFind
+	} else if (!currentNode[direction]) {
+		return false;
+
+	// If neither of those conditions are true, keep recursively going down the BinarySearchTree
+	} else {
+		return recursivelyCheckIfIncludesNode(currentNode[direction], numToFind);
 	}
 }
 
@@ -54,10 +87,17 @@ function includesNode(currentNode, numToFind) {
 // let myBinarySearchTree = new BinarySearchTree(myRootNode);
 
 // *****BINARY TREE AFTER INSERTIONS*****
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 12);
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 14);
-// myBinarySearchTree.insertNode(myBinarySearchTree.root, 16);
+// myBinarySearchTree.insertNode(12);
+// myBinarySearchTree.insertNode(14);
+// myBinarySearchTree.insertNode(16);
+
+// let emptyBinarySearchTree = new BinarySearchTree();
+// emptyBinarySearchTree.insertNode(1);
+// emptyBinarySearchTree.insertNode(2);
 
 // *****FINDING THINGS IN BINARY TREE*****
-// myBinarySearchTree.includesNode(myBinarySearchTree.root, 14);
-// myBinarySearchTree.includesNode(myBinarySearchTree.root, 2);
+// myBinarySearchTree.checkIfIncludesNode(16);
+// myBinarySearchTree.checkIfIncludesNode(100);
+
+// let anotherEmptyBinarySearchTree = new BinarySearchTree();
+// anotherEmptyBinarySearchTree.checkIfIncludesNode(5);
